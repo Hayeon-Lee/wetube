d685b1c108486309222f3672be61a07a53408756
Hayeon-Lee <papepopepe@naver.com>
2023-09-29 15:56:09 +0900

    feat: make parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.send("Home page Videos");
-export const see = (req, res) => res.send("Watch Video");
+export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
