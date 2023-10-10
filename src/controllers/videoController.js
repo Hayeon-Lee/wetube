709b70d77e1ab01d1497337068d14cc78c83adbf
Hayeon-Lee <papepopepe@naver.com>
2023-10-11 02:02:44 +0900

    feat: Search Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos: videos });
   } catch (err) {
     console.log(err);
@@ -68,4 +68,15 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: { $regex: new RegExp(keyword, "i") },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
