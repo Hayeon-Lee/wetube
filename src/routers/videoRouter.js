000f8754c35b3ffd3e072e36bb19843d807b28f9
Hayeon-Lee <papepopepe@naver.com>
2023-10-11 01:28:04 +0900

    feat: Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,7 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
-  //deleteVideo,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -14,5 +14,6 @@ const videoRouter = express.Router();
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
 export default videoRouter;
