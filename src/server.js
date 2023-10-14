0f76a625f0b405a0771729829ae611fa4af308c0
Hayeon-Lee <papepopepe@naver.com>
2023-10-14 11:55:40 +0900

    feat: Make Sessions with Cookies

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import session from "express-session";
 
 const app = express(); //create express application
 
@@ -16,6 +17,19 @@ app.use(logger);
 
 app.use(express.urlencoded({ extended: true }));
 //post에 body 속성이 추가되고 저장된다
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: "true",
+    saveUninitialized: true,
+  })
+);
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id} ${req.session.potato}`);
+});
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
